25455ae5f01d90bdd17c5fe9a7bce3611fe09671
Mochi <mochitreets0304@gmail.com>
2025-02-19 19:16:59 +0800

    2/19 router每日練習

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter, NavLink } from 'react-router-dom';
+import { HashRouter, NavLink, Routes, Route } from 'react-router-dom';
 
 const Todo = () => {
   return <p>這是 Todo 頁面</p>;
@@ -30,8 +30,15 @@ function App() {
           </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
+        <Routes>
+          <Route path="/" element={<p>這是首頁</p>} />
+          <Route path="/register" element={<Register />} /> 
+          <Route path="/login" element={<Login />} /> 
+          <Route path="/todo" element={<Todo />} /> 
+        </Routes>
         {/* 練習區 */}
       </HashRouter>
+      
     </div>
   );
 }
